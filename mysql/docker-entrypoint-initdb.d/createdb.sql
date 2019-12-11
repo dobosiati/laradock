3ee309ee16dd2b339ad42698c0a7ee37bb1bc4ed
Attila Dobosi <dobosiati@gmail.com>
2019-12-11 12:07:44 +1000

    modified  crontab and db file

--- mysql/docker-entrypoint-initdb.d/createdb.sql
+++ mysql/docker-entrypoint-initdb.d/createdb.sql
@@ -22,31 +22,8 @@ GRANT ALL ON `homestead`.* TO 'homestead'@'%' ;
 CREATE DATABASE IF NOT EXISTS `q` COLLATE 'utf8_general_ci' ;
 GRANT ALL ON `q`.* TO 'homestead'@'%' ;
 
-CREATE DATABASE IF NOT EXISTS `screensavers` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `screensavers`.* TO 'homestead'@'%' ;
+-- CREATE DATABASE IF NOT EXISTS `screensavers` COLLATE 'utf8_general_ci' ;
+-- GRANT ALL ON `screensavers`.* TO 'homestead'@'%' ;
 
-CREATE DATABASE IF NOT EXISTS `greeta` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `greeta`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `greeta_testing` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `greeta_testing`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `taskey` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `taskey`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `taskey_testing` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `taskey_testing`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `taskey_dusk` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `taskey_dusk`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `surprise` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `surprise`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `surprise_testing` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `surprise_testing`.* TO 'homestead'@'%' ;
-
-CREATE DATABASE IF NOT EXISTS `laravel_websockets` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `laravel_websockets`.* TO 'homestead'@'%' ;
 
 FLUSH PRIVILEGES ;
